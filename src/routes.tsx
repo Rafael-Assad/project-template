7fcd2e3e53ee4ae65e082977a6eeb4f307d2b498
Rafael Assad <rafael.assad1010@gmail.com>
2023-05-16 13:13:19 -0300

    Feat: added routes.tsx file and setted the standarts

---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import {Routes as Switch, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route path='/'
+                element={ <Home/> }
+            />
+
+            <Route path='/about'
+                element={ <About/> }
+            />
+        </Switch>
+    )
+}
+
+export default Routes
